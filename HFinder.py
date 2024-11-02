9b681761622b881281ad3786c4626789087727a9
Sanchit <work.sanchitdang@gmail.com>
2024-11-01 22:21:17 -0400

    functions updated

--- HFinder.py
+++ HFinder.py
@@ -1,6 +1,4 @@
 import InstaDataSyncer
-import threading
-
 
 # Functions
 def make_follow_following_data():
@@ -8,22 +6,18 @@ def make_follow_following_data():
     InstaDataSyncer.get_acc_you_follow()
     InstaDataSyncer.get_acc_following_you()
 
-    f3 = open(r'synced_data/acc_following_you.txt', 'r')
-    f4 = open(r'synced_data/acc_you_follow.txt', 'r')
-    f5 = open(r'synced_data/not_following_back.txt', 'w')
-    f6 = open(r'synced_data/you_are_not_following_back.txt', 'w')
+    acc_following_you = open(r'synced_data/acc_following_you.txt', 'r')
+    acc_you_follow = open(r'synced_data/acc_you_follow.txt', 'r')
+    not_following_back = open(r'synced_data/not_following_back.txt', 'w')
+    you_are_not_following_back = open(r'synced_data/you_are_not_following_back.txt', 'w')
 
-    followers_list = f3.readlines()
-    following_list = f4.readlines()
+    followers_list = acc_following_you.readlines()
+    following_list = acc_you_follow.readlines()
 
     for following in following_list:
-        # f5.write('People You Are Following, But They Are Not Following you Back!!\n')
         if following not in followers_list:
-            # print(following, end='')
-            f5.write(following)
+            not_following_back.write(following)
 
     for followers in followers_list:
-        # f6.write('People You Are Not Following Back!!\n')
         if followers not in following_list:
-            # print(followers, end='')
-            f6.write(followers)
+            you_are_not_following_back.write(followers)
--- Unfollow_ppl_function.py
+++ Unfollow_ppl_function.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
 from time import sleep
 
-# inc_in_progress = 0
-
-
 def unfollow_asses():
     options = webdriver.ChromeOptions()
     options.add_experimental_option("debuggerAddress", "localhost:9222")
@@ -12,9 +9,7 @@ def unfollow_asses():
 
     # Unfollow multiple
     with open(r'synced_data\not_following_back.txt', 'r') as not_following_back_data:
-        # global inc_in_progress
         total_user_names = len(not_following_back_data.readlines())
-        # inc_in_progress = 100 / total_user_names
         not_following_back_data.seek(0)
         for i in range(total_user_names):
             user_name = not_following_back_data.readline()
@@ -22,13 +17,9 @@ def unfollow_asses():
             driver.get('https://www.instagram.com/' + user_name_without_newline)
             try:
                 sleep(2)
-                # driver.save_screenshot("ss"+str(i)+".png")
                 driver.find_element_by_xpath('//span[@aria-label="Following"]').click()
                 sleep(1)
                 driver.find_element_by_xpath('//button[text()="Unfollow"]').click()
-                # inc_in_progress += inc_in_progress
-                # OR (EASY), split gui_main into functions
-                # gui_main.my_progress['value'] += inc_in_progress
                 sleep(2)
             except:
                 pass
